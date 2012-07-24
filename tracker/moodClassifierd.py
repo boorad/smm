d4d151aa05402a0c7834aba4f8690aea1a710f52
Brad Anderson <brad@sankatygroup.com>
2012-07-24 18:00:59 -0400

    whitespace

--- tracker/moodClassifierd.py
+++ tracker/moodClassifierd.py
@@ -20,36 +20,36 @@ HOST, PORT = "", 6666
 
 
 class TCPRequestHandler(SocketServer.BaseRequestHandler):
-    
+
     BUFSIZE = 1440
-    
+
     def _send(self,data_to_send):
-        
+
         data = cPickle.dumps(data_to_send, protocol=2)
         dataLen = "%032d" % len(data)
         self.request.send(dataLen)
         self.request.sendall(data)
-        
-    
+
+
     def handle(self):
         dataLen = int(self.request.recv(32))
-        
+
         raw_data = ""
-        
+
         while True:
             raw_data += self.request.recv(self.BUFSIZE)
             if len(raw_data) >= dataLen:
                 break
-            
-        
+
+
         if raw_data:
             searchPhrase, recvData = cPickle.loads(str(raw_data))
-        
-        
+
+
         self.server.moodCls.setUpSearchPhraseCleaner(searchPhrase)
-           
+
         try:
-        
+
             data_to_send = []
             for r in recvData:
                 text = r.get('text')
@@ -59,9 +59,9 @@ class TCPRequestHandler(SocketServer.BaseRequestHandler):
         except Exception,e:
             raise e
             return False
-            
+
         self._send(data_to_send)
-            
+
 
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
@@ -69,7 +69,7 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     allow_reuse_address = True
     max_children = 40
     timeout = 300
-    
+
     @staticmethod
     def loadCls():
         ThreadedTCPServer.langCls = LangDetect(supportedLangs)
@@ -83,7 +83,7 @@ class serverDaemon(Daemon):
             searchserver.serve_forever()
         except KeyboardInterrupt:
             sys.exit(0)
-        
+
 
 
 
@@ -92,10 +92,10 @@ if __name__ == "__main__":
     PID = os.path.join(os.getcwd(),'moodClassifierd.pid')
     stdout = os.path.join(os.getcwd(),'moodClassifierd.log')
     stderr = os.path.join(os.getcwd(),'moodClassifierd.log')
-    
+
     daemon = serverDaemon(pidfile=PID,stderr=stderr,stdout=stdout)
-    
-    
+
+
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             print 'starting...'
@@ -112,21 +112,19 @@ if __name__ == "__main__":
             print 'starting debug mode...'
             ThreadedTCPServer.loadCls()
             print 'OK'
-            
-            
+
+
             searchserver = ThreadedTCPServer((HOST, PORT), TCPRequestHandler)
             try:
                 searchserver.serve_forever()
             except KeyboardInterrupt:
                 sys.exit(0)
-            
+
         else:
             print "Unknown command"
             sys.exit(2)
             sys.exit(0)
-            
+
     else:
         print "usage: %s start|stop|restart" % sys.argv[0]
         sys.exit(2)
-
-
